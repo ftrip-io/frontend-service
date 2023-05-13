2286f03fafebf8d0782ce24540ee2d20c24300e1
Albert Makan <68916222+albertmakan@users.noreply.github.com>
2023-05-13 15:06:41 +0200

    ci: add Dockerfile, develop-merge and semantic versioning workflows (#10)
    * ci: add Dockerfile, develop-merge and semantic versioning workflows
    
    * fix: PR suggestions

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/ftrip-io/frontend-service",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        assets: ["docs/CHANGELOG.md"],
+      },
+    ],
+  ],
+};
