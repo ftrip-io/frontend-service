c43f491b13562c2e90b2413c226c9ee0e8d66bca
Panic <panic.milos99@gmail.com>
2023-06-07 17:27:26 +0200

    feat: added hook for users map.

---
+++ core/utils/map.ts
@@ -0,0 +1,12 @@
+export const createEntitiesMap = (
+  entities: any,
+  keySelector = (entity: any) => entity.id,
+  valueSelector = (entity: any) => entity
+) => {
+  return (
+    entities?.reduce((map: any, entity: any) => {
+      map[keySelector(entity)] = valueSelector(entity);
+      return map;
+    }, {}) ?? {}
+  );
+};
