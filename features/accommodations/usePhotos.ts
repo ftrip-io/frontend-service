1eba3d304592015b2c78a86a55c7fc88ec16c0fd
Luka Bjelica <52993990+bjelicaluka@users.noreply.github.com>
2023-06-24 17:40:15 +0200

    fix: window object access (#24)

--- features/accommodations/usePhotos.ts
+++ features/accommodations/usePhotos.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import getConfig from "next/config";
+import { useEffect, useState } from "react";
 const { publicRuntimeConfig: config } = getConfig();
 
 export function usePhotos(id: string, dependencies: any[] = [], updated?: string[]) {
@@ -12,13 +13,17 @@ export function usePhotos(id: string, dependencies: any[] = [], updated?: string
     }
   );
 
+  const [path, setPath] = useState("/photoService/");
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window?.location?.host === "localhost") {
+      setPath(`${config.imageServicePath}/`);
+    }
+  }, []);
+
   return {
     photoUrls: (updated ?? (data?.data as string[]) ?? []).map((p) =>
-      p.startsWith("http")
-        ? p
-        : window?.location?.host === "localhost"
-        ? `${config.imageServicePath}/${p}`
-        : `/photoService/${p}`
+      p.startsWith("http") ? p : `${path}${p}`
     ),
     isLoading: isFetching,
     error: (error as any)?.response?.data,
