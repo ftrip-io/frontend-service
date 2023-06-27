b9a9a26fc6c1b43093ccfc94c5669c2505ae4d0d
Dragana Filipović <58399353+draganaF@users.noreply.github.com>
2023-06-28 00:35:14 +0200

    feat: added cards for requests and reservations (#26)
    * feat: added cards for requests and reservations
    
    * fix: fixed dates

--- features/requests/requestActions.ts
+++ features/requests/requestActions.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 import type { CreateReservationRequest } from "./ReservationRequestsModels";
+import moment from "moment";
 
 function createReservationRequest(request: CreateReservationRequest) {
+  request.datePeriod.dateFrom = moment(request.datePeriod.dateFrom).startOf("day").format() as any;
+  request.datePeriod.dateTo = moment(request.datePeriod.dateTo).startOf("day").format() as any;
+
   return axios.post(`/bookingService/api/reservation-requests`, request);
 }
 
