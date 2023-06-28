a6cf3539eb72a76974e7179d889295effb46c281
Albert Makan <68916222+albertmakan@users.noreply.github.com>
2023-06-28 09:48:46 +0200

    fix: image service path, added new Dockerfile (#28)

--- features/accommodations/usePhotos.ts
+++ features/accommodations/usePhotos.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import getConfig from "next/config";
-import { useEffect, useState } from "react";
 const { publicRuntimeConfig: config } = getConfig();
 
 export function usePhotos(id: string, dependencies: any[] = [], updated?: string[]) {
@@ -13,17 +12,9 @@ export function usePhotos(id: string, dependencies: any[] = [], updated?: string
     }
   );
 
-  const [path, setPath] = useState("/photoService/");
-
-  useEffect(() => {
-    if (typeof window !== "undefined" && window?.location?.host === "localhost") {
-      setPath(`${config.imageServicePath}/`);
-    }
-  }, []);
-
   return {
     photoUrls: (updated ?? (data?.data as string[]) ?? []).map((p) =>
-      p.startsWith("http") ? p : `${path}${p}`
+      p.startsWith("http") ? p : `${config.imageServicePath}/${p}`
     ),
     isLoading: isFetching,
     error: (error as any)?.response?.data,
